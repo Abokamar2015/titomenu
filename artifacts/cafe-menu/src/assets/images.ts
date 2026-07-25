d509e78a2c9577abdd7ebc5130eb2a550aa8365b
Abokamar2015 <stitou.galaxy.hs@gmail.com>
2026-07-25 23:20:05 +0300

    Tito Menu Full Project

--- artifacts/cafe-menu/src/assets/images.ts
+++
@@ -1,7 +0,0 @@
-export const IMAGES = {
-  COVER: "/images/cover.jpg",
-  LOGO: "/images/LOGO.png",
-  CROPPED: "/images/cropped_image_0_1777473782186812804.jpg",
-} as const;
-
-export type ImageKey = keyof typeof IMAGES;
--- lib/db/src/schema/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./menuItems";
-export * from "./categories";
-export * from "./settings";
